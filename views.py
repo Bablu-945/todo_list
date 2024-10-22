6d14ac2ee87dafe4376158cbd033e210e59cbb5a
DINESH CHILUKAMARI <chiluka9064@gmail.com>
2024-10-22 12:10:06 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+
+def signup(request):
+   if request.method=='POST':
+    fnm =request.POST.get('fnm')
+    emailid=request.POST.get('email')
+    pwd=request.POST.get('pwd')
+    
+    return render(request,'signup.html')
